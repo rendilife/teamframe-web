16b572b281efc8754fe140760f9a312d6b32165f
Rene Janku <teamframedev@gmail.com>
2026-02-21 22:48:53 +0100

    Website update

--- app/page.tsx
+++ app/page.tsx
@@ -260,22 +260,21 @@ export default function Home() {
           {/* 2: Machines */}
 
 
-
-        <Reveal>
-          <TwoCol
-            title="Reálný incident – dlouhodobý prostoj"
-            intro="Dashboard počítá prostoj na základě reálného času od změny stavu. Incident zůstává aktivní i po restartu aplikace a pokračuje ve výpočtu bez ohledu na otevření modulu."
-            bullets={[
-              "Výpočet trvání od okamžiku změny stavu",
-              "Perzistence i po restartu dashboardu",
-              "Zobrazení v ACTIVE pohledu",
-              "Podklad pro měsíční agregace",
-            ]}
-            shot={shots.find((s) => s.id === "incident7h")!}
-            onOpen={() => open("incident7h")}
-            reverse={false}
-          />
-        </Reveal>
+            <Reveal>
+              <TwoCol
+                title="Reálný incident – dlouhodobý prostoj"
+                intro="Dashboard počítá prostoj na základě reálného času od změny stavu. Incident zůstává aktivní i po restartu aplikace a pokračuje ve výpočtu bez ohledu na otevření modulu."
+                bullets={[
+                  "Výpočet trvání od okamžiku změny stavu",
+                  "Perzistence i po restartu dashboardu",
+                  "Zobrazení v ACTIVE pohledu",
+                  "Podklad pro měsíční agregace",
+                ]}
+                shot={shots.find((s) => s.id === "graphs")!}
+                onOpen={() => open("graphs")}
+                reverse={false}
+              />
+            </Reveal>
 
           <Reveal>
             <TwoCol
