01646abb6baddede8598a59224d4116012695ec0
Rene Janku <teamframedev@gmail.com>
2026-02-24 18:24:35 +0100

    Website update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Script from "next/script";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -17,9 +17,7 @@ export const metadata: Metadata = {
   title: "TeamFrame – plánovač směn (8h/12h) a výrobní dashboard",
   description:
     "TeamFrame je modulární systém pro plánování směn, řízení strojů, kvalifikace zaměstnanců a evidenci incidentů pro výrobní provoz.",
-  icons: {
-    icon: "/favicon.ico",
-  },
+  icons: { icon: "/favicon.ico" },
 };
 
 export default function RootLayout({
@@ -29,24 +27,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="cs">
-      <head>
-        {/* Google Analytics */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-80VYCX7QV9"
-          strategy="afterInteractive"
-        />
-        <Script id="ga4" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-80VYCX7QV9');
-          `}
-        </Script>
-      </head>
-
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
+
+        {/* Google Analytics (GA4) */}
+        <GoogleAnalytics gaId="G-80VYCX7QV9" />
       </body>
     </html>
   );
